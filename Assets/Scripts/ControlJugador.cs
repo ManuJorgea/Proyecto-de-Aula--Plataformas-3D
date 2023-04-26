f0c7519b26e001d72c9689eebf9a1a83faa4a5e2
jeronimo velasquez <jerov79@gmail.com>
2023-04-26 07:42:14 -0500

    Movimiento frontal jugador

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ControlJugador : MonoBehaviour
 {
+    public float speed = 20f;
+    public float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class ControlJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Registro del inputs de las teclas
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        /// Mueve el carro en sentido vertical
+        transform.Translate(Vector3.right * -1 * Time.deltaTime * speed * forwardInput);
+        /// Rota el carro basado en el sentido horizontal
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
